eca3e64bf29c6ec412e354cfd3ff2ba24bbbe50f
dymeneutron <diegoluciano.carvalho@gmail.com>
2022-11-19 16:10:02 -0300

    commit do banco completo

---
+++ src/Conta.java
@@ -0,0 +1,56 @@
+
+public abstract class Conta implements Iconta {
+	
+	
+	private static final int AGENCIA_PADRAO = 0;
+	private static int SEQUENCIAL = 0;
+	protected Cliente cliente;
+	
+	protected int agencia;
+	protected int numero;
+	protected double saldo;
+	
+	public Conta(Cliente cliente) {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
+	}
+
+
+	@Override
+	public void sacar(double valor) {
+		saldo -= valor;
+		
+	}
+	@Override
+	public void depositar(double valor) {
+		saldo += valor;
+		
+	}
+	@Override
+	public void transferir(double valor, Conta contaDestino) {
+		this.sacar(valor);
+		contaDestino.depositar(valor);
+		
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	protected void imprimirInfosComuns() {
+		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+		System.out.println(String.format("Agencia: %d", this.agencia));
+		System.out.println(String.format("numero: %d", this.numero));
+		System.out.println(String.format("Saldo: %.2f", this.saldo));
+	}
+	
+}
+
+
